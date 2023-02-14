00462d0ec2178313a89899586ad9a5707cba040a
Tom Blockley <tom.e.blockley@gmail.com>
2023-02-14 17:46:15 +0000

    Force apns usage

--- src/ios/PushPlugin.m
+++ src/ios/PushPlugin.m
@@ -315,19 +315,20 @@
             NSLog(@"FCM Sender ID %@", fcmSenderId);
 
             //  GCM options
+            //  GCM options
             [self setFcmSenderId: fcmSenderId];
-            if(isGcmEnabled && [[self fcmSenderId] length] > 0) {
-                NSLog(@"Using FCM Notification");
-                [self setUsesFCM: YES];
-                dispatch_async(dispatch_get_main_queue(), ^{
-                    if([FIRApp defaultApp] == nil)
-                        [FIRApp configure];
-                    [self initRegistration];
-                });
-            } else {
-                NSLog(@"Using APNS Notification");
-                [self setUsesFCM:NO];
-            }
+            //if(isGcmEnabled && [[self fcmSenderId] length] > 0) {
+            //    NSLog(@"Using FCM Notification");
+            //    [self setUsesFCM: YES];
+            //    dispatch_async(dispatch_get_main_queue(), ^{
+            //        if([FIRApp defaultApp] == nil)
+            //            [FIRApp configure];
+            //        [self initRegistration];
+            //    });
+            //} else {
+            NSLog(@"Using APNS Notification");
+            [self setUsesFCM:NO];
+            //}
             id fcmSandboxArg = [iosOptions objectForKey:@"fcmSandbox"];
 
             [self setFcmSandbox:@NO];
